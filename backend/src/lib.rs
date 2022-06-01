653fbc57ee26524be60fc017d0e531264e4decd3
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-06-01 17:02:32 +0900

    fix: .env

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,37 @@
+use once_cell::sync::Lazy;
+use serde::Deserialize;
+
 pub mod auth;
 pub mod domain;
 pub mod infrastructure;
 pub mod media;
 pub mod session;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub mariadb_database: String,
+    pub mariadb_user: String,
+    pub mariadb_password: String,
+    pub mariadb_address: String,
+    pub mariadb_port: u32,
+    pub redis_url: String,
+    pub frontend_origin: String,
+}
+
+impl Config {
+    pub fn get_database_url(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}/{}",
+            self.mariadb_user,
+            self.mariadb_password,
+            self.mariadb_address,
+            self.mariadb_port,
+            self.mariadb_database
+        )
+    }
+}
+
+pub static CONFIG: Lazy<Config> = Lazy::new(|| {
+    dotenv::dotenv().unwrap();
+    envy::from_env().unwrap()
+});
