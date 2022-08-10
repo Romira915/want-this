afe8b78b34612fb2ff09c1f9e6b948db385a3cfa
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-08-10 16:28:19 +0900

    fix: cargo fix and cargo clippy

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,15 +1,4 @@
-use want_this_frontend::component::cta::Cta;
-use want_this_frontend::component::header::Header;
-use want_this_frontend::{App, CONFIG};
-use wasm_bindgen::{JsCast, JsValue};
-use wasm_bindgen_futures::JsFuture;
-use web_sys::RequestCredentials;
-use web_sys::{Request, RequestInit, RequestMode, Response};
-use yew::{html::Scope, prelude::*};
-use yew_hooks::prelude::*;
-
-use yew_hooks::use_async;
-use yew_router::prelude::*;
+use want_this_frontend::App;
 
 fn main() {
     let config = wasm_logger::Config::new(if cfg!(debug_assertions) {
--- frontend/src/route.rs
+++ frontend/src/route.rs
@@ -1,5 +1,5 @@
-use yew::{html::Scope, prelude::*};
-use yew_router::prelude::*;
+
+
 
 pub mod login;
 pub mod main;
