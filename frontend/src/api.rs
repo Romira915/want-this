ce942a7b1c7700fe1b1756a5e1508919405347e1
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-08-19 12:45:24 +0900

    fix: fetch

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -1,23 +1,9 @@
+use reqwasm::http::Request;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestCredentials, RequestInit, RequestMode, Response};
 
 pub(crate) async fn fetch(url: &str) -> Result<String, JsValue> {
-    let mut opts = RequestInit::new();
-    opts.method("GET")
-        .mode(RequestMode::Cors)
-        .credentials(RequestCredentials::Include);
+    let resp = Request::get(url).send().await;
 
-    let request = Request::new_with_str_and_init(url, &opts)?;
-    log::info!("request");
-
-    let window = gloo_utils::window();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-    log::info!("resp_value");
-
-    let resp: Response = resp_value.dyn_into().unwrap();
-    let text = JsFuture::from(resp.text()?).await?;
-    log::info!("text");
-
-    Ok(text.as_string().unwrap())
+    todo!()
 }
